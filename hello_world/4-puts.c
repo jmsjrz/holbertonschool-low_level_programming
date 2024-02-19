e8b6ad37aaf53013a694d84cefcff68245e4821c
James <8708@holbertonstudents.com>
2024-02-19 15:55:38 +0100

    fix commentary to main

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-/* Function that puts a string and return 0 */
+/**
+ * main -  Function that puts a string and return 0
+ */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
-return (0); }
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
