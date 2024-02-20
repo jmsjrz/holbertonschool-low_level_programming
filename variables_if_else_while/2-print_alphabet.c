9742aeeacbb3ff2e45549b2885bc15cb89f9b8dd
James <8708@holbertonstudents.com>
2024-02-20 14:40:16 +0100

    add task 2 program

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercase using putchar function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+	for (a = 'a' ; a <= 'z' ; a++) {
+		putchar(C);
+	}
+	return (0);
+}
+
