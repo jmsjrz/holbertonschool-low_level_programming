a77b9acdc0075c1767dffcb5fb109b8ab4ba35d6
James <8708@holbertonstudents.com>
2024-02-20 15:35:31 +0100

    add task 3 program

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,15 @@
 int main(void)
 {
 	char a;
-
+	char A;
 	for (a = 'a' ; a <= 'z' ; a++)
 	{
 		putchar(a);
 	}
-	for (a = 'A' ; a <= 'Z' ; a++)
+	for (A = 'A' ; A <= 'Z' ; A++)
+	{
+		putchar(A);
+	}
 	putchar('\n');
 	return (0);
 }
