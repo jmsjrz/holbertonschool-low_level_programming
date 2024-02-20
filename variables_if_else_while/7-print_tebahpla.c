abc34eb849feac9d5c7c47757fe5c411fd450741
James <8708@holbertonstudents.com>
2024-02-20 18:36:26 +0100

    add task 7 program

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercase using putchar function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z' ; a <= 'a' ; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
