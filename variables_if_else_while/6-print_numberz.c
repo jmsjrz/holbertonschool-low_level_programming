7dd1d6452cf7a225e54a16dbecfa6feeea644ef0
James <8708@holbertonstudents.com>
2024-02-20 18:31:53 +0100

    fix style

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,12 @@
  */
 int main(void)
 {
-    int a;
+int a;
 
-    for (a = 0; a < 10; a++)
-    {
-        putchar('0' + a); 
-    }
-    putchar('\n');
-    return (0);
+for (a = 0; a < 10; a++)
+{
+putchar('0' + a);
+}
+putchar('\n');
+return (0);
 }
