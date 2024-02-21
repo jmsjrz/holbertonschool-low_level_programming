7b80778d3ae56b8e986c2c1cc7c3d23966ea5ecd
James <8708@holbertonstudents.com>
2024-02-21 15:47:46 +0100

    add task 1 program

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Prints a string with _putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for(print_alphabet = 'a' ; print_alphabet <= 'z' ; print_alphabet++);
+	{
+	_putchar(print_alphabet);
+	}
+	_putchar('\n');
+	return (0);
+}
