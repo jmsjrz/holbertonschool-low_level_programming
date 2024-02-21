4c85dff6560017a3c83da0bc9aa273cc98e386ed
James <8708@holbertonstudents.com>
2024-02-21 09:59:46 +0100

    fix style

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -4,23 +4,23 @@
 
 /**
  * main - prints all the combinations of single digit numbers
- * 
+ *
  * Return: Always 0
  */
 int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 0; num <= 9; num++)
-    {
-        putchar('0' + num);
-        if (num < 9)
-        {
-            putchar(',');  
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return 0;
+	for (num = 0; num <= 9; num++)
+	{
+	putchar('0' + num);
+	if (num < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
 }
 
