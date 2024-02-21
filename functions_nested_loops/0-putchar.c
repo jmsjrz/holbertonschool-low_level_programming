768b013f7079ee697311f520145ecde03907f91e
James <8708@holbertonstudents.com>
2024-02-21 15:13:32 +0100

    add task 0 program

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - Prints a string with printf
+ * main - Prints a string with _putchar
  * Return: Always 0
  */
 
 int main(void)
 {
-printf("_putchar\n");
+_putchar("_putchar\n");
 return (0);
 }
