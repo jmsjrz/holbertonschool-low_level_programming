c636d55e33dc545ae8f5809c385baa97e77e3c15
James <8708@holbertonstudents.com>
2024-02-22 17:15:48 +0100

    fix betty style

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints all the alphabet 10 times
+ * print_alphabet_x10 - Prints all the alphabet 10 times
  * Return: Always 0
  */
 void print_alphabet_x10(void)
