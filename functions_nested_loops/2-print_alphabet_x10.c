ba9b5a6c7826e3e7e4fed2ccdf2f8663288a8e6e
James <8708@holbertonstudents.com>
2024-02-22 09:51:44 +0100

    add task 2 program

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints all the alphabet 10 times
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
+
