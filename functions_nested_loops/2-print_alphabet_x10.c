5dd542ec3bcbb7c0caa8a76f6a3ed984aa6ccdee
James <8708@holbertonstudents.com>
2024-02-22 12:38:48 +0100

    fix syntax

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 	char letter;
 	int number;
 
-	for (number = 0 ; number < 10 ; number++) 
+	for (number = 0 ; number < 10 ; number++)
 	{
-		for (letter = 'a' ; letter <= 'z' ; letter++) 
+		for (letter = 'a' ; letter <= 'z' ; letter++)
 		{
 			_putchar(letter);
-		}		
-		_putchar('\n'); 
+		}
+		_putchar('\n');
 	}
 }
 
