73be6532294c011dd472d1542eacce5178e8d7b5
James <8708@holbertonstudents.com>
2024-02-23 15:07:46 +0100

    add task 7 program

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @int r: number to be checked
+ * Return: 0
+ */
+int print_last_digit(int)
+{
+int r;
+
+print_last_digit(98);
+print_last_digit(0);
+
+r = print_last_digit(-1024);
+
+_putchar('0' + r);
+_putchar('\n');
+return (0);
+}
