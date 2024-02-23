d023656561e57a2ca9dae79744f651eb9d390670
James <8708@holbertonstudents.com>
2024-02-23 10:38:41 +0100

    add task 5 program

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for alphabetic character
+ * 
+ * Return: 0 if alphabetic character, else 1
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+		return (0)
+		printf(0);
+	else if (n < 0)
+		return ('-1')
+		printf('-');
+	else 
+		return (1)
+		printf('+');
+}
+
