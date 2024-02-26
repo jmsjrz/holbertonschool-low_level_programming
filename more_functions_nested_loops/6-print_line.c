4c3d9630a3730cb29467411fd413b2d972024950
James <8708@holbertonstudents.com>
2024-02-26 14:58:39 +0100

    add task 6 program

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_line - prints a line in the terminal
+* @n: parameter
+* Return: nothing
+*/
+void print_line(int n)
+{
+	for (int number = 0; number < n; number++)
+
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
