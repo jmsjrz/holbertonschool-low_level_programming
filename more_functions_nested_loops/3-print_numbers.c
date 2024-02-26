d4b0ca72ead89dea8f2b8b7d6ea5e65a5ef91999
James <8708@holbertonstudents.com>
2024-02-26 11:16:08 +0100

    add task 3 program

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints a string
- * Return: Always 0
- */
-
-int main(void)
+ *print_alphabet_x10 - Prints all the alphabet 10 times
+ *Return: Always 0
+*/
+void print_alphabet_x10(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	int number;
+
+	{
+		for (number = '0' ; number <= '9' ; number++)
+		{
+			_putchar(number);
+		}
+		_putchar('\n');
+	}
 }
