f417b8cc1ee0bc4b6b9ebee2420335c594ebdbaf
James <8708@holbertonstudents.com>
2024-02-26 16:12:52 +0100

    add task 8 program

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_square - prints a square
+* @size: size of the square
+* Return: nothing
+*/
+void print_square(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
