5af94f498f6c6c4019b9b4c54fff43b15d65727c
James <8708@holbertonstudents.com>
2024-02-26 11:17:18 +0100

    add task 3 program

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10 - Prints all the alphabet 10 times
+ *print_numbers - Prints all the alphabet 10 times
  *Return: Always 0
 */
-void print_alphabet_x10(void)
+void print_numbers(void)
 {
 	int number;
 
