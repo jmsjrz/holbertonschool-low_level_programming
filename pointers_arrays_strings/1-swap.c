edf98ff5bf39e118c2d360e28c2ea7b36092ec62
James <8708@holbertonstudents.com>
2024-02-27 14:04:24 +0100

    add task 1 program

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - swap two int value eachothers
+* @a: pointer to an int
+* @b: pointer to an int
+* Return: nothing
+*/
+void swap_int(int *a, int *b)
+{
+	int temporaryvar = *a
+
+	*a = *b;
+	*b = temporaryvar;
+}
