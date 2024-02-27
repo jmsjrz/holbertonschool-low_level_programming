b80ea43a2c5d0584f1bfdd3cba50ecb2a6fdb7e6
James <8708@holbertonstudents.com>
2024-02-27 15:06:08 +0100

    add task 3 program

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - print a string
+* @str: pointer to the string
+* Return: 0
+*/
+void _puts(char *str)
+{
+	for (int i = 0; str[i] != '\0'; i++)
+
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
