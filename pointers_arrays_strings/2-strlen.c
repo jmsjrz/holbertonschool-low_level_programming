451b7b27ab2a6a6ea9a49645d722461b48210d67
James <8708@holbertonstudents.com>
2024-02-27 14:41:58 +0100

    add task 2 program

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strlen - return the length of a string
+* @s: pointer to char
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int count = 0;
+
+	for (count = 0; s[count] ; count++)
+	return (count);
+}
