022cab766314e3febeb6871349a69a94a4c23fd7
James <8708@holbertonstudents.com>
2024-02-29 16:29:17 +0100

    add task 10 program

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,21 +1,13 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src
- * @dest: destination
- * @src: source
- * Return: pointer to dest
- */
+* _atoi - convert a string to an int
+* @s: string to be converted
+* Return: pointer to dest
+*/
 
-char *_strcpy(char *dest, char *src)
+int _atoi(char *s)
 {
-	int index;
-
-	for (index = 0; src[index] != 0; index++)
-	{
-		dest[index] = src[index];
-	}
-	dest[index] = '\0';
-	return (dest);
+	printf("Hello World");
+	return (0);
 }
