7cdb12927985d2ed902bc395e6c6b8190d91772d
James <8708@holbertonstudents.com>
2024-03-02 14:35:49 +0100

    add task 3 program

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,22 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
- */
+* _strcmp - compare two strings
+* @s1: string 1 to be compared
+* @s2: string 2 to be compared
+* Return: return the difference
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (0);
+
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
