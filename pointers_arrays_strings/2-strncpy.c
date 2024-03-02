e8ef2a827e6edcca0db0bb1655dd1f82522fab6f
James <8708@holbertonstudents.com>
2024-03-02 14:27:00 +0100

    add task 2 program

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,32 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
+ * _strncpy - copy the string
+ * @dest: destination string
+ * @src: source string
+ * @n: int
+ * Return: pointer dest
  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index1 = 0;
+	int index2 = 0;
+
+	while (src[index2] != '\0' && index2 < n)
+	{
+		dest[index1] = src[index2];
+		index1++;
+		index2++;
+	}
+	if (index2 < n)
+	{
+		while (index1 < n)
+		{
+			dest[index1] = '\0';
+			index1++;
+		}
+	}
+	return (dest);
+
+}
