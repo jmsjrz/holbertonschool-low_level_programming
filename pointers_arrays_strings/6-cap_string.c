6035dc8fa3a71eacdbc9eb96410db42ece164ea9
James <8708@holbertonstudents.com>
2024-03-03 17:36:18 +0100

    add task 6 program

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * 
- * 
- * 
- */
+* cap_string - check the code
+*
+* @s : pointer
+*
+* Return: Always 0.
+*/
+
+char *cap_string(char *s)
+{
+
+
+	return (s);
+}
