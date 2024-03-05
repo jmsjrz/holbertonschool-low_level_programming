ccb487efc422e0d6bdf49beba370f1ee1e23334c
James <8708@holbertonstudents.com>
2024-03-05 10:43:31 +0100

    fix betty documentation

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
- * _strpbrk - Searches a string for any of a set of bytes.
- * @s: The string to be searched.
- * @accept: The set of bytes to search for.
- * Description: Locates the first occurrence in the string s
- * of any of the bytes in the string accept.
- * Returns: A pointer to the byte in s that matches one of the bytes
- * in accept, or NULL if no such byte is found.
- */
+/**
+* _strpbrk - searches a string for any of a set of bytes.
+* This function scans the string s for the first occurrence of any
+* of the bytes in the string accept.
+* @s: string to be searched.
+* @accept: set of bytes to search for.
+* Return: pointer to the byte in s that matches one of the bytes in accept,
+* or NULL if no such byte is found.
+*/
 char *_strpbrk(char *s, char *accept)
 {
 	char *a;
