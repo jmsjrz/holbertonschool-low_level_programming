40221d83d47b853633405b32bb3048fb97f24a45
James <8708@holbertonstudents.com>
2024-04-02 11:09:00 +0200

    add task 1 program

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+/**
+ * list_len - return the numbers of elements in a linked list
+ * @h: pointer to the head
+ * Return: numbers of elements in a linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t lenght = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		lenght++;
+	}
+	return (lenght);
+}
