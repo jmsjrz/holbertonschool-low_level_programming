890cd0ba54355a1ba1bb12e51bd6820dbbefa627
James <8708@holbertonstudents.com>
2024-04-03 13:37:08 +0200

    fix variables declaration and specifier

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,26 +1,36 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
-* print_list - prints all the elements of a list_t list.
-* @h: pointer to the start of the list.
+* print_list - Prints all the elements of a list_t list.
+* @h: A pointer to the head of the list_t list.
 *
-* Return: the number of nodes in the list.
+* Description: Iterates over each node of a singly linked list
+* of type list_t, and prints the string contained in each node,
+* followed by the length of the string. If the string is NULL,
+* the function prints `[0] (nil)`. The function traverses the
+* list until the end is reached.
+*
+* Return: The total number of nodes in the list.
 */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t node_count = 0; /* Node counter */
+	const list_t *current_node = h;
 
-	while (h != NULL)
+	while (current_node != NULL)
 	{
-		if (h->str == NULL)
+		if (current_node->str == NULL) /* Check if the string is NULL */
+		{
 			printf("[0] (nil)\n");
+		}
 		else
-			printf("[%zu] %s\n", h->len, h->str);
+		{
+			printf("[%lu] %s\n", current_node->len, current_node->str);
+		}
 
-		h = h->next;
-		nodes++;
+		current_node = current_node->next; /* Move to the next node in the list */
+		node_count++; /* Increment node counter */
 	}
 
-	return (nodes);
+	return (node_count);
 }
