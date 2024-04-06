0010d378eb744897dbfdfa9e0fc6a8fcd5edd74a
James <8708@holbertonstudents.com>
2024-04-06 17:51:00 +0200

    update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,13 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
-#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
-
-int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 #endif
